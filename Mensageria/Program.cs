a4bd9775d666c2738c458b678a2225b47c162d92
Robert A. dos Anjos <116765770+Robert-A-Anjos@users.noreply.github.com>
2023-09-01 00:21:57 -0300

    incluído template email

--- Mensageria/Program.cs
+++ Mensageria/Program.cs
@@ -4,6 +4,7 @@ using System.Net.Mail;
 using System.Collections.Generic;
 using System.IO;
 using OfficeOpenXml;
+using Mensageria;
 
 namespace MassEmailSenderExample
 {
@@ -19,7 +20,19 @@ namespace MassEmailSenderExample
             string smtpHost = "smtp.gmail.com";
             int smtpPort = 587;
 
-            string excelFilePath = @"C:\Users\robert.alves\source\aulas\AulasEuCodo\ReadFiles\dadostoemail.xlsx"; // Caminho para o arquivo Excel
+
+
+            // Aqui deve ser informado o caminho da planilha excel, que será usada com base dos dados para enviar os emails
+            string excelFilePath = @"C:\Users\robert.alves\source\aulas\AulasEuCodo\ReadFiles\dadostoemail.xlsx";
+
+
+            string saudacao = DateTime.Now.Hour switch
+            {
+                int horaAtual when horaAtual > 4 && horaAtual <= 11 => "Bom Dia,",
+                int horaAtual when horaAtual >= 12 && horaAtual <= 18 => "Boa Tarde,",
+                _ => "Boa Noite,"
+            };
+
 
             try
             {
@@ -35,20 +48,31 @@ namespace MassEmailSenderExample
 
                     for (int row = 2; row <= rowCount; row++)
                     {
+                        //abaixo estão as colunas na planilha
                         string bloco = worksheet.Cells[row, 1].Value?.ToString();
                         string unidade = worksheet.Cells[row, 2].Value?.ToString();
                         string corretor = worksheet.Cells[row, 3].Value?.ToString();
                         string cliente = worksheet.Cells[row, 4].Value?.ToString();
                         string gerente = worksheet.Cells[row, 5].Value?.ToString();
                         string clienteEmail = worksheet.Cells[row, 6].Value?.ToString();
+                        string empreendimento = worksheet.Cells[row, 7].Value?.ToString();
 
-                        string emailBody = $"Olá {cliente},\n\nSegue abaixo as informações:\n\n" +
-                            $"Bloco: {bloco}\nUnidade: {unidade}\nCorretor: {corretor}\nGerente: {gerente}";
+
+
+                        // Supondo que você tenha a classe TemplateEmail e o método GetHtmlTemplate definidos
+
+                        // Instancie a classe TemplateEmail
+                        TemplateEmail templateEmail = new TemplateEmail();
+
+                        // Agora você pode usar a variável emailBody para compor o corpo do seu e-mail
+                        // e enviá-lo usando a biblioteca ou método de envio de e-mails de sua escolha
+
 
                         MailMessage mail = new MailMessage(senderEmail, clienteEmail)
                         {
-                            Subject = "Informações da unidade",
-                            Body = emailBody
+                            Subject = $@"PASTA {empreendimento.ToUpper()} | Corretor: {corretor}",
+                            Body = templateEmail.GetHtmlTemplate(cliente, corretor, gerente, bloco, unidade, empreendimento, saudacao),
+                            IsBodyHtml = true
                         };
 
                         // Construir o caminho do arquivo com base no número da unidade
---
+++ Mensageria/TemplateEmail.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mensageria
+{
+    public class TemplateEmail
+    {
+
+        public string GetHtmlTemplate
+            (
+                string cliente,
+                string corretor,
+                string gerente,
+                string bloco,
+                string unidade,
+                string empreendimento,
+                string saudacao
+            )
+        {
+            return $@"
+<!DOCTYPE html>
+<html lang=""en"">
+<head>
+<meta charset=""UTF-8"">
+<meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
+<title>Template de Email</title>
+<style>
+  body {{
+    margin: 0;
+    padding: 0;
+    font-family: Roboto;
+    color: #333333; /* Cor cinza escura */
+    background-color: #f4f4f4;
+  }}
+  .container {{
+    width: 100%;
+    max-width: 600px;
+    margin: 0 auto;
+    background-image: url('https://innovabr.com.br/wp-content/uploads/2022/11/apartamento-edge-cambui-campinas-sp-entrada-2.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    border-radius: 5px;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.8);
+    overflow: hidden;
+  }}
+  .header {{
+    text-align: center;
+    padding: 10px 20px;
+    background-color: rgba(255, 255, 255, 0.8);
+    border-radius: 5px 5px 0 0;
+  }}
+  .logo {{
+    max-width: 200px;
+    margin: 0 auto; /* Centralize a logo */
+  }}
+  .Gerente {{
+    text-align: center; /* Centralize o texto do gerente */
+    color: #2b4272; /* Cor específica */
+  }}
+  .content {{
+    padding: 20px;
+    background-color: rgba(255, 255, 255, 0.8);
+    border-radius: 5px;
+    margin-top: 10px; /* Ajuste conforme necessário */
+    margin: 15%;
+  }}
+  .footer {{
+    text-align: center;
+    padding: 20px 0;
+    font-size: 12px;
+    color: #888888;
+    background-color: rgba(255, 255, 255, 0.8);
+  }}
+p {{
+    font-size: 15px !important;
+  }}
+</style>
+</head>
+<body>
+  <div class=""container"">
+    <div class=""header"">
+      <img src=""https://innovabr.com.br/wp-content/uploads/2021/12/LOGO-PSD-N-Colorido-PARA-FUNDO-BRANCO-1.png"" alt=""innovabr"" class=""logo"">
+    </div>
+    <div class=""content"">
+      <p>Empreendimento: <b>{empreendimento}</b> -  Torre: <b>{bloco}</b> | Unidade: <b>{unidade}</b></p>
+      <p> {saudacao} <b>{cliente}</b>, conforme alinhado com <b>{corretor}</b>, segue a pasta em <b class=""Gerente"">anexo</b>!</p>
+     
+    </div>
+
+    <div class=""footer"">
+      <p class=""Gerente"" >cc: Gerente Imobiliário <b>{gerente}</b></p>
+      <p><b>©InnovaBR. Todos os direitos reservados.</b></p>
+    </div>
+  </div>
+</body>
+</html>
+";
+
+        }
+    }
+}
