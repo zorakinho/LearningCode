d20e426e93bde8dab2f063dc1238ad8e0a570e93
Robert A. dos Anjos <116765770+Robert-A-Anjos@users.noreply.github.com>
2023-09-10 17:06:47 -0300

    Conversão Áudio e Vídeo: OK

--- YoutubeConvert/Program.cs
+++ YoutubeConvert/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using YoutubeExplode;
 using YoutubeExplode.Videos.Streams;
@@ -8,42 +10,85 @@ class Program
 {
     static async Task Main(string[] args)
     {
+
+        // URL do vídeo do Youtube
+        string videoUrl = "https://www.youtube.com/watch?v=BF0uf7apZDQ";
+
         var youtube = new YoutubeClient();
 
+
+        var audioInfo = await youtube.Videos.GetAsync(videoUrl);
+
+        var streamInfoSet = await youtube.Videos.Streams.GetManifestAsync(audioInfo.Id);
+
+        var audioStreamInfo = streamInfoSet
+            .GetAudioOnlyStreams()
+            .GetWithHighestBitrate();
+
+
         // Insira o link do vídeo do YouTube que você deseja baixar
-        string videoUrl = "https://www.youtube.com/watch?v=BF0uf7apZDQ";
+        
 
         // Obtém o manifest das streams
         var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoUrl);
 
         // Seleciona a melhor stream de áudio (a de maior taxa de bits)
-        var audioStreamInfo = streamManifest
+        var videoStreamInfo = streamManifest
             .GetAudioStreams()
             .Where(s => s.Container == Container.Mp4) // Filtro para streams MP4
             .GetWithHighestBitrate();
 
-        if (audioStreamInfo != null)
+        if (videoStreamInfo != null)
         {
             // Caminho para o diretório do projeto
             string diretorioDoProjeto = Directory.GetCurrentDirectory();
 
-            // Pasta onde você deseja salvar o arquivo final (no caso, o MP3)
+            // Pasta onde você deseja salvar o arquivo final (MP4 e MP3)
             string pastaDeDestino = diretorioDoProjeto;
 
-            // Nome do arquivo final (MP3)
-            string nomeDoArquivoFinal = "video.mp4";
+            // Obtém informações do vídeo para obter o título
+            var videoInfo = await youtube.Videos.GetAsync(videoUrl);
+
+            // Título do vídeo (limpo para remover caracteres inválidos em nomes de arquivo)
+            string tituloDoVideo = GetSafeFileName(videoInfo.Title);
+
+            // Nome do arquivo final para o vídeo MP4
+            string nomeDoArquivoFinalVideo = $"{tituloDoVideo}.mp4";
+
+            // Nome do arquivo final para o áudio MP3
+            string nomeDoArquivoFinalAudio = $"{tituloDoVideo}.mp3";
+
+            // Caminho completo para o arquivo MP4
+            string caminhoDoMp4 = Path.Combine(pastaDeDestino, nomeDoArquivoFinalVideo);
 
             // Caminho completo para o arquivo MP3
-            string caminhoDoMp3 = Path.Combine(pastaDeDestino, nomeDoArquivoFinal);
+            string caminhoDoMp3 = Path.Combine(pastaDeDestino, nomeDoArquivoFinalAudio);
 
             // Download da stream de áudio (MP4)
+            await youtube.Videos.Streams.DownloadAsync(videoStreamInfo, caminhoDoMp4);
             await youtube.Videos.Streams.DownloadAsync(audioStreamInfo, caminhoDoMp3);
 
-            Console.WriteLine("URL convertida para video");
+            // Converta o arquivo MP4 em MP3 usando FFmpeg
+            //ConvertMp4ToMp3(caminhoDoMp4, caminhoDoMp3);
+
+            Console.WriteLine("URL convertida para Vídeo MP4 com sucesso!");
+            Console.WriteLine("URL convertida para Áudio MP3 com sucesso!");
         }
         else
         {
             Console.WriteLine("Não foi possível encontrar uma versão de áudio para o vídeo.");
         }
     }
+
+    // Função para obter um nome de arquivo seguro removendo caracteres inválidos
+    static string GetSafeFileName(string fileName)
+    {
+        return string.Join("_", fileName.Split(Path.GetInvalidFileNameChars()));
+    }
+
+    // Função para converter um arquivo MP4 em MP3 usando FFmpeg
+    // Função para converter um arquivo MP4 em MP3 usando FFmpeg
+
 }
+
+
